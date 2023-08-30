a08b564663dca8ee17a9a4ab323f550366fb219d
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-08-30 19:07:39 +0200

    Add Nameable class and name correction decorators

---
+++ nameable.rb
@@ -0,0 +1,26 @@
+class Nameable
+    def correct_name(name)
+      raise NotImplementedError
+    end
+  end
+
+  class BaseDecorator < Nameable
+    attr_accessor :nameable
+
+    def initialize(nameable)
+      super()
+      @nameable = nameable
+    end
+
+    def correct_name
+      @nameable.correct_name
+    end
+  end
+
+  class CapitalizeDecorator < BaseDecorator
+    def correct_name
+      @nameable.correct_name.capitalize
+    end
+  end
+
+
