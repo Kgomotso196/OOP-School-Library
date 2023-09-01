785c87c299156ec693f2a33cb266a84480d43430
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-09-01 21:43:20 +0200

    Refactor display logic for individuals

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,9 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_accessor :specialization
+  attr_accessor :name, :specialization
 
-  def initialize(age, specialization, name, parent_permission: true)
+  def initialize(age, name, specialization, parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
